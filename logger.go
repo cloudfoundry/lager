9da38dd349ee6222db7723248684e4e25eeefa54
Ted Young <tyoung@pivotallabs.com>
2014-05-21 17:15:12 -0700

    completed implementation of Logger interface
    Signed-off-by: Ted Young <tyoung@pivotallabs.com>

--- logger.go
+++ logger.go
@@ -2,12 +2,18 @@ package lager
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
+
+const STACK_TRACE_BUFFER_SIZE = 1024 * 100
 
 type Logger interface {
 	RegisterSink(Sink)
 	Debug(task, action, description string, data ...Data)
+	Info(task, action, description string, data ...Data)
+	Error(task, action, description string, err error, data ...Data)
+	Fatal(task, action, description string, err error, data ...Data)
 }
 
 type logger struct {
@@ -35,7 +41,7 @@ func (l *logger) Debug(task, action, description string, data ...Data) {
 	logData["description"] = description
 
 	log := LogFormat{
-		Timestamp: fmt.Sprintf("%.9f", time.Now().UnixNano()),
+		Timestamp: currentTimestamp(),
 		Source:    l.component,
 		Message:   fmt.Sprintf("%s.%s.%s", l.component, task, action),
 		LogLevel:  DEBUG,
@@ -45,4 +51,80 @@ func (l *logger) Debug(task, action, description string, data ...Data) {
 	for _, sink := range l.sinks {
 		sink.Log(DEBUG, log.ToJSON())
 	}
+}
+
+func (l *logger) Info(task, action, description string, data ...Data) {
+	var logData Data
+	if len(data) > 0 {
+		logData = data[0]
+	}
+
+	logData["description"] = description
+
+	log := LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    l.component,
+		Message:   fmt.Sprintf("%s.%s.%s", l.component, task, action),
+		LogLevel:  INFO,
+		Data:      logData,
+	}
+
+	for _, sink := range l.sinks {
+		sink.Log(INFO, log.ToJSON())
+	}
+}
+
+func (l *logger) Error(task, action, description string, err error, data ...Data) {
+	var logData Data
+	if len(data) > 0 {
+		logData = data[0]
+	}
+
+	logData["description"] = description
+	logData["error"] = err.Error()
+
+	log := LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    l.component,
+		Message:   fmt.Sprintf("%s.%s.%s", l.component, task, action),
+		LogLevel:  ERROR,
+		Data:      logData,
+	}
+
+	for _, sink := range l.sinks {
+		sink.Log(ERROR, log.ToJSON())
+	}
+}
+
+func (l *logger) Fatal(task, action, description string, err error, data ...Data) {
+	var logData Data
+	if len(data) > 0 {
+		logData = data[0]
+	}
+
+	stackTrace := make([]byte, STACK_TRACE_BUFFER_SIZE)
+	stackSize := runtime.Stack(stackTrace, false)
+	stackTrace = stackTrace[:stackSize]
+
+	logData["description"] = description
+	logData["error"] = err.Error()
+	logData["trace"] = string(stackTrace)
+
+	log := LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    l.component,
+		Message:   fmt.Sprintf("%s.%s.%s", l.component, task, action),
+		LogLevel:  FATAL,
+		Data:      logData,
+	}
+
+	for _, sink := range l.sinks {
+		sink.Log(FATAL, log.ToJSON())
+	}
+
+	panic(err)
+}
+
+func currentTimestamp() string {
+	return fmt.Sprintf("%.9f", float64(time.Now().UnixNano())/1e9)
 }
--- logger_test.go
+++ logger_test.go
@@ -1,7 +1,10 @@
 package lager_test
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/pivotal-golang/lager"
 
@@ -18,27 +21,124 @@ var _ = Describe("Logger", func() {
 	var action = "my-action"
 	var description = "my-description"
 	var logData = lager.Data{"foo": "bar"}
+	var logs []lager.LogFormat
 
 	BeforeEach(func() {
 		logger = lager.NewLogger(component)
 		testSink = lager.NewTestSink()
 		logger.RegisterSink(testSink)
+		logs = nil
 	})
+
+	var TestForCommonLogFeatures = func() {
+		var log lager.LogFormat
+
+		BeforeEach(func() {
+			log = logs[0]
+		})
+
+		It("writes a log to the sink", func() {
+			Ω(logs).Should(HaveLen(1))
+		})
+
+		It("records the source component", func() {
+			Ω(log.Source).Should(Equal(component))
+		})
+
+		It("outputs a properly-formatted message", func() {
+			Ω(log.Message).Should(Equal(fmt.Sprintf("%s.%s.%s", component, task, action)))
+		})
+
+		It("has a timestamp", func() {
+			expectedTime := float64(time.Now().UnixNano()) / 1e9
+			parsedTimestamp, err := strconv.ParseFloat(log.Timestamp, 64)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(parsedTimestamp).Should(BeNumerically("~", expectedTime, 1.0))
+		})
+
+		It("data contains the description", func() {
+			Ω(log.Data["description"]).Should(Equal(description))
+		})
+
+		It("data contains custom user data", func() {
+			Ω(log.Data["foo"]).Should(Equal("bar"))
+		})
+	}
 
 	Describe("Debug", func() {
 		BeforeEach(func() {
 			logger.Debug(task, action, description, logData)
+			logs = testSink.Logs()
 		})
 
-		It("writes the proper log format", func() {
-			logs := testSink.Logs()
+		TestForCommonLogFeatures()
 
-			Ω(logs).Should(HaveLen(1))
-
+		It("sets the proper output level", func() {
 			Ω(logs[0].LogLevel).Should(Equal(lager.DEBUG))
-			Ω(logs[0].Source).Should(Equal(component))
-			Ω(logs[0].Message).Should(Equal(fmt.Sprintf("%s.%s.%s", component, task, action)))
-			Ω(logs[0].Data).Should(Equal(lager.Data{"description": description, "foo": "bar"}))
+		})
+	})
+
+	Describe("Info", func() {
+		BeforeEach(func() {
+			logger.Info(task, action, description, logData)
+			logs = testSink.Logs()
+		})
+
+		TestForCommonLogFeatures()
+
+		It("sets the proper output level", func() {
+			Ω(logs[0].LogLevel).Should(Equal(lager.INFO))
+		})
+
+	})
+
+	Describe("Error", func() {
+		var err = errors.New("oh noes!")
+
+		BeforeEach(func() {
+			logger.Error(task, action, description, err, logData)
+			logs = testSink.Logs()
+		})
+
+		TestForCommonLogFeatures()
+
+		It("sets the proper output level", func() {
+			Ω(logs[0].LogLevel).Should(Equal(lager.ERROR))
+		})
+
+		It("data contains error message", func() {
+			Ω(logs[0].Data["error"]).Should(Equal(err.Error()))
+		})
+	})
+
+	Describe("Fatal", func() {
+		var err = errors.New("oh noes!")
+		var fatalErr interface{}
+		BeforeEach(func() {
+			defer func() {
+				fatalErr = recover()
+				logs = testSink.Logs()
+			}()
+
+			logger.Fatal(task, action, description, err, logData)
+		})
+
+		TestForCommonLogFeatures()
+
+		It("sets the proper output level", func() {
+			Ω(logs[0].LogLevel).Should(Equal(lager.FATAL))
+		})
+
+		It("data contains error message", func() {
+			Ω(logs[0].Data["error"]).Should(Equal(err.Error()))
+		})
+
+		It("data contains stack trace", func() {
+			Ω(logs[0].Data["trace"]).ShouldNot(BeEmpty())
+		})
+
+		It("panics with the provided error", func() {
+			Ω(fatalErr).Should(Equal(err))
 		})
 	})
 })

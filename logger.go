81d18ed6a75f16276fed99134f0e8397ec2e91e2
Ted Young <tyoung@pivotallabs.com>
2015-01-06 15:46:18 -0800

    add SessionName to Logger interface
    ...for now...
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

--- logger.go
+++ logger.go
@@ -12,6 +12,7 @@ const STACK_TRACE_BUFFER_SIZE = 1024 * 100
 type Logger interface {
 	RegisterSink(Sink)
 	Session(task string, data ...Data) Logger
+	SessionName() string
 	Debug(action string, data ...Data)
 	Info(action string, data ...Data)
 	Error(action string, err error, data ...Data)
@@ -39,6 +40,10 @@ func NewLogger(component string) Logger {
 
 func (l *logger) RegisterSink(sink Sink) {
 	l.sinks = append(l.sinks, sink)
+}
+
+func (l *logger) SessionName() string {
+	return l.task
 }
 
 func (l *logger) Session(task string, data ...Data) Logger {
